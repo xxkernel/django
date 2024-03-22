8784685f1e74e4cc3eae514ee07fde5abe0d0174
HiyoRitsu <122852098+HiyoRitsu@users.noreply.github.com>
2024-03-22 14:27:41 +0600

    django

---
+++ news/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import News, Category
+
+def index(request):
+    news = News.objects.all()
+    context = {
+        'news': news, 
+        'title': 'Список новостей',
+    }
+    return render(request, 'news/index.html', context)
+
+def get_category(request, category_id):
+    news = News.objects.filter(category_id=category_id)
+    category = Category.objects.get(pk=category_id)
+    return render(request, 'news/category.html', {'news': news, 'category':category})
