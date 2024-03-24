74be367e7e58ca628ada2a4ec08afac63a1f710a
HiyoRitsu <122852098+HiyoRitsu@users.noreply.github.com>
2024-03-24 09:51:45 +0600

    forms

--- news/forms.py
+++ news/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from .models import News
+import re
+from django.core.exceptions import ValidationError
 
 class NewsForm(forms.ModelForm):
     class Meta:
@@ -12,4 +14,8 @@ class NewsForm(forms.ModelForm):
             'category': forms.Select(attrs={'class': 'form-control', 'rows':5})
         }
 
-
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if re.match(r'\d', title):
+            raise ValidationError('Название не должно начинаться с цифры')
+        return title
