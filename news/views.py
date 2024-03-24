44eea5e215d76c2ec6d9f78a178bf09e1473feef
HiyoRitsu <122852098+HiyoRitsu@users.noreply.github.com>
2024-03-24 08:52:50 +0600

    forms

--- news/views.py
+++ news/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import News, Category
+from .forms import NewsForm
 
 def index(request):
     news = News.objects.all()
@@ -19,3 +20,14 @@ def view_news(request, news_id):
     # news_item = News.objects.get(pk=news_id)
     news_item = get_object_or_404(News, pk=news_id)
     return render(request, 'news/view_news.html', {'news_item': news_item})
+
+def add_news(request):
+    if request.method=='POST':
+        form = NewsForm(request.POST)
+        if form.is_valid():
+            # print(form.cleaned_data)
+            news = News.objects.create(**form.cleaned_data, )
+            return redirect(news)
+    else:
+        form = NewsForm()
+    return render(request, 'news/add_news.html', {'form':form})
